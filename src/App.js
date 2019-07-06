3395e711ee3ec790dd4571aae7b15c0d5e02482c
jeffau13 <jeffau13@gmail.com>
2019-07-06 17:56:03 -0400

    set up routes with react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" exact component={Rooms} />
+        <Route path="/rooms/:slug" exact component={SingleRoom} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
