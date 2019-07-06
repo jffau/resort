dfe54a1d890fa70df07d8fb4115b223c3358ac42
jeffau13 <jeffau13@gmail.com>
2019-07-06 18:35:39 -0400

    added navbar component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/rooms" exact component={Rooms} />
         <Route path="/rooms/:slug" exact component={SingleRoom} />
         <Route component={NotFound} />
+        {/* Route with no match will always "match", so route for NotFound will render  */}
       </Switch>
     </>
   );
