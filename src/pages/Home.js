c2a38ee1fd2d31574eaa16d70cac908c0c4f1839
jeffau13 <jeffau13@gmail.com>
2019-07-08 15:41:23 -0400

    added service section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
-import { Link } from 'react-router-dom';
+
+import Services from '../components/Services';
 const Home = () => {
   return (
-    <Hero hero="defaultHero">
-      <Banner title="Beach Resort" subtitle="Luxurious Rooms starting at $99">
-        <Link to="/rooms" className="btn-primary">
-          Check out Rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="defaultHero">
+        <Banner title="Beach Resort" subtitle="Luxurious Rooms starting at $99">
+          <Link to="/rooms" className="btn-primary">
+            Check out Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
