7b78c3858bcb86e53cbc6d70a6896fee1ec4e194
jeffau13 <jeffau13@gmail.com>
2019-07-08 17:25:32 -0400

    method for formatting data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
+import Rooms from '../pages/Rooms';
 export default class FeaturedRooms extends Component {
   /*
   This subscribes the component to RoomContext, 
@@ -8,6 +9,13 @@ export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     const value = this.context;
-    return <div>{value.greeting + ' ' + value.name}</div>;
+    let { rooms } = this.context;
+    return (
+      <div>
+        {rooms.map(room => {
+          return room.name;
+        })}
+      </div>
+    );
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-
+import items from './data';
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
   state = {
-    greeting: 'hello',
-    name: 'john'
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
   };
+
+  formatData(data) {
+    let tempItems = data.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
+  // getting local data:
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    console.log(rooms);
+    this.setState({ rooms, loading: false });
+  }
+
   render() {
     return (
       <RoomContext.Provider value={{ ...this.state }}>
